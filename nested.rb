efcd5c2cbd86e67d8d6ebe19bec2954c232b3155
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-21 22:52:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash[:]
+     programmer_hash[:yukihiro_matsumoto] = {}
+     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     
 end
 
